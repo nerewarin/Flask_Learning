1acdf64d87fe77ec208ec98a00fae900734ba5de
nerewarin <nerewarin89@mail.ru>
2015-07-08 19:47:40 +0500

    Heroku created

--- flask_probe01/routes.py
+++ flask_probe01/routes.py
@@ -6,6 +6,7 @@ from flask import Flask, url_for, request, render_template
 from flask_probe01 import app
 from functools import wraps
 import redis
+import random
 from warnings import warn
 
 # def my_decorator(f):
@@ -20,28 +21,55 @@ from warnings import warn
 # r = redis.StrictRedis(host="localhost", port=6379, db=0)
 # add parameters to cast responses to unicode
 r = redis.StrictRedis(host="localhost", port=6379, db=0, charset="UTF-8", decode_responses=True)
+# check redis service is available
 
+try:
+    response = r.client_list()
+except redis.ConnectionError:
+    #your error handlig code here
+    warn("You need to start redis servise from task manager (Windows)")
+else:
+    print "redis is active"
+    # print "redis response = %s" % response
 
 # we will name initially route url as server
 # with a app.route we can expand urls
+# print "\nkeys"
+# print [key for key in r.keys()]
+print "\nwhat in DB now?"
+keys = r.keys()
+for key in keys:
+    # print ".", key
+    # print "_", r.get(key), "\n"
+    # print "key.split(\":\")", key.split(":")[0]
+    # delete empty questions
+    if len(key.split(":")[0])  < 1 :
+        # print "AHAAA", key
+        r.delete(key)
+        # r.
+for key in keys:
+    print ".", key
+    print "_", r.get(key), "\n"
+
+# # keys = [key.encode("UTF-8") for key in r.keys()]
+# vals = r.mget(keys)
+# print "\nvalues"
+#
+# for val in vals:
+#     # for v in val:
+#     #     print v
+#     print vals
+# # kv = zip(keys, vals)
+# # print kv
+
 
 # server/
 @app.route('/')
 def hello():
-
-    # check redis service is available
-    try:
-        response = r.client_list()
-    except redis.ConnectionError:
-        #your error handlig code here
-        warn("You need to start redis servise from task manager (Windows)")
-    else:
-        print "redis response = %s" % response
-
-
     # href is a link tag
     # url_for('create') = calling create func
     createLink = "<a href='" + url_for('create') + "'>Create a question</a>"
+    print " url_for('create')",  url_for('create')
     return """<html>
                 <head>
                     <title> hello head! </title>
@@ -56,7 +84,7 @@ def hello():
 
 
 # @app.route('/create')
-@app.route('/create!', methods=["GET", "POST"])
+@app.route('/create', methods=["GET", "POST"])
 def create():
      # return "<h2>This is the create page!</h2>"
     if request.method == "GET":
@@ -64,6 +92,7 @@ def create():
         return render_template('CreateQuestion.html')
 
     elif request.method == "POST":
+        print "create - post"
         # read form data and save it
         title = request.form["title"]
         question = request.form["question"]
@@ -87,10 +116,15 @@ def create():
         else:
             print "ok redis.set %s:question = %s, %s:answer = %s" % (title, question, title, answer)
 
-        return render_template('CreatedQuestion.html', question = question)
+        # let user redirect to random question after he add his question
+        rnd_question = random.choice(r.keys()).split(":")[0]
+        return render_template('CreatedQuestion.html', question = question, rnd_question = rnd_question)
+
+    # elif request:
     else:
 
         return "<h2>Invalid request! unknown method %s for route('/create')</h2>" % request.method
+
 
 
 # server/question/<title>
@@ -102,9 +136,7 @@ def question(title):
         # send the user the form
         question =  r.get(title+':question')
 
-
         # read question from data store
-
         return render_template('AnswerQuestion.html', question = question)
 
     elif request.method == "POST":
@@ -122,7 +154,7 @@ def question(title):
         except:
             raise Exception, "answer %s has type %s and has no method .lower()" % (submittedAnswer, type(submittedAnswer))
 
-        print "submittedAnswer %s answer = %s" % (submittedAnswer, answer)
+        print "submittedAnswer = %s, answer = %s" % (submittedAnswer, answer)
         if answer.lower() == submittedAnswer.lower():
             return "Correct!"
             # return render_template("Correct.html")
